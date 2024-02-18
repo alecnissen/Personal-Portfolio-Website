a96f36b9897d126ffacebc6b87376dde2bfef039
alecnissen <alecnissen@yahoo.com>
2024-02-18 14:29:41 -0500

    began adding content and base styling for welcome title section, now will work on creating the animated react logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
+// import './App.css'
+import '../src/Styles/index.scss'
 import Header from './Components/Header'
-
+import WelcomeText from './Components/WelcomeText'
 
 function App() {
 
   return (
     <>
   <Header></Header>
+  <WelcomeText></WelcomeText>
     </>
   )
 }
---
+++ src/Components/WelcomeText.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import laptopImg from "../assets/laptop.png";
+
+function WelcomeText() {
+  return (
+    <> 
+   <main> 
+    <div className='welcome-title-container'> 
+        <div className='welcome-title-text'> 
+        <h3>Welcome to my Portfolio</h3>
+        </div>
+
+        <div className='name-title-container'> 
+        <h1>Hello! My name is Alec and I am a Frontend Developer!</h1>
+        <p>Welcome to my site! My passion is to build amazing user interfaces.Here you can find my projects which showcase my ability!</p>
+        </div>
+
+    </div>
+        {/* <img src={laptopImg}></img> */}
+   </main>
+   </>
+  )
+}
+
+export default WelcomeText
