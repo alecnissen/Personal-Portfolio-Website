351fb65e983b3dd63a34ebe450dbc26c37ac5489
alecnissen <alecnissen@yahoo.com>
2024-02-23 13:09:03 -0500

    added typing animation in welcome text

--- src/Components/WelcomeText.jsx
+++ src/Components/WelcomeText.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect }  from 'react'
 import laptopImg from "../assets/laptop.png";
 
 function WelcomeText() {
+
+  const [displayedText, setDisplayedText] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0); // Add a state variable for current index
+
+  const fullText = "Hello! My name is Alec and I am a Frontend Developer!";
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (currentIndex < fullText.length) {
+        setDisplayedText(prevText => prevText + fullText[currentIndex]);
+        setCurrentIndex(prevIndex => prevIndex + 1); // Increment current index
+      } else {
+        clearInterval(interval);
+      }
+    }, 100); // Adjust typing speed as needed
+
+    return () => clearInterval(interval);
+  }, [currentIndex]); // Re-run effect whenever currentIndex changes
+
   return (
     <> 
    <main> 
@@ -11,7 +30,8 @@ function WelcomeText() {
         </div>
 
         <div className='name-title-container'> 
-        <h1>Hello! My name is Alec and I am a Frontend Developer!</h1>
+        {/* <h1>Hello! My name is Alec and I am a Frontend Developer!</h1> */}
+        <h1>{displayedText}</h1>
         <p>Welcome to my site! My passion is to build amazing user interfaces. Here you can find my projects which showcase my ability!</p>
         </div>
 
