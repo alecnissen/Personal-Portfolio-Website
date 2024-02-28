29c345f71111c716066f680c1094e61897edf65e
alecnissen <alecnissen@yahoo.com>
2024-02-28 12:06:25 -0500

    added project section an animation so when that section is focused on it will apply an animation

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,14 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 // import fashionGemsDemo from "../assets/fashion-gems-site-demo.png"
 import memoryCardDemo from "../assets/memory-card-cropped-2.png"
 import fashionGemsDemo from "../assets/tablet-view.png"
 import cvAppDemo from "../assets/cv-app-4.png"
 import HoverClassProjectCards from './HoverClassProjectCards';
+import "../Styles/projects_section_styles.scss";
 
 
 function Projects() {
 
   const [show, setShow] = useState(false);
+  const projectsRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5, // Trigger when 50% of the projects container is visible
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setShow(true);
+          observer.disconnect(); // Disconnect the observer once triggered
+        }
+      });
+    }, options);
+
+    if (projectsRef.current) {
+      observer.observe(projectsRef.current);
+    }
+
+    return () => {
+      if (projectsRef.current) {
+        observer.unobserve(projectsRef.current);
+      }
+    };
+  }, []);
 
 
 
@@ -20,8 +49,8 @@ function Projects() {
         Here you can see my progress, all the trial and error, and hands on learning that took place on each of these Projects! 
         </p>
 
-    <div className='projects-container'> 
-        <div className='shopping-cart-container'> 
+    <div className='projects-container' ref={projectsRef}> 
+        <div className={`shopping-cart-container ${show ? 'focused' : ''}`}> 
         {/* <h3>FashionGems Store</h3> */}
       <div className='modal-content'>
 
@@ -40,7 +69,7 @@ function Projects() {
 
         </div>
 
-        <div className='memory-card-container'> 
+        <div className={`memory-card-container ${show ? 'focused' : ''}`}> 
         {/* <h3>Memory Card Game</h3> */}
         <div className='modal-content-memory-card'>
 
@@ -54,7 +83,7 @@ function Projects() {
         <img src={memoryCardDemo} className='memory-card-demo-styles'></img>
         </div>
 
-        <div className='cv-app-container'> 
+        <div className={`cv-app-container ${show ? 'focused' : ''}`}> 
         {/* <h3>CV/Resume Generator</h3> */}
         <div className='modal-content-cv-app'>
 
