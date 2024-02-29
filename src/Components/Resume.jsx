78f549fb45c64324e84c04478257d034a57ed68d
alecnissen <alecnissen@yahoo.com>
2024-02-28 23:03:10 -0500

    got the animation for the resume section working, used the entire wrapper so the headers are included in the animation, moving onto footer animation

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -2,15 +2,50 @@ import React from 'react'
 import emoji from "../assets/emoji.png"
 import downloadIcon from "../assets/icons8-download-64.png"
 import msgIcon from "../assets/msg-icon.png"
+import { useRef, useEffect, useState } from 'react'
 
 function Resume() {
+
+  const resumeContainer = useRef(null);
+  const [show, setShow] = useState(false);
+
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5,
+    };
+  
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setShow(true);
+          observer.disconnect();
+        }
+      });
+    }, options);
+  
+    if (resumeContainer.current) {
+      observer.observe(resumeContainer.current);
+    }
+  
+    return () => {
+      if (resumeContainer.current) {
+        observer.unobserve(resumeContainer.current);
+      }
+    };
+  }, []);
+
   return (
     <>
-    <div className='resume-content-wrapper'>
+    {/* <div className={`resume-content-wrapper ${show} ? 'focused' : ''}`} ref={resumeContainer}> */}
+    <div className={`resume-content-wrapper ${show ? 'focused' : ''}`} ref={resumeContainer}>
     <h1>Resume</h1>
     <h1>Interested in working with me?</h1>
+    {/* <div className={`resume-download-container ${show ? 'focused' : ''}`} ref={resumeContainer}> */}
 
-    <div className='resume-download-container'>
+    <div className={'resume-download-container'}>
       
       <img src={emoji} className='emoji-styles'></img>
       <div className='text-container'> 
