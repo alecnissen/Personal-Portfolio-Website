db83617c676b1e5b29afed938577f5b81516a3bb
alecnissen <alecnissen@yahoo.com>
2024-03-02 21:21:10 -0500

    having difficulty having user download resume from btn click, moving onto linear gradient background, saving work

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -52,12 +52,12 @@ function Resume() {
       <p>If you still have any questions, feel free to contact me. Additionally, you can download my resume below!</p>
       
       <div className='resume-contact-links-container'> 
-          <a href='../assets/mock-resume-portfolio.png.docx' download={true}>
+          {/* <a href='../assets/Alec-Nissen-Resume.docx' download> */}
         <button>
         <img src={downloadIcon} className="download-icon-styles"></img>
         Download CV
         </button>
-        </a>
+        {/* </a> */}
         <a href='https://www.linkedin.com/in/alec-nissen-9562a4b5/'>
         <button>
           <img src={msgIcon} className='msg-icon-styles'></img>
